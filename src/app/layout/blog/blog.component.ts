1674212997c1d83759d0a1150fa8cfbbc13bc675
Pedro Jorge <skitvandarken2013@gmail.com>
2025-08-11 18:36:02 +0100

    logos telcables, logo ac

--- src/app/layout/blog/blog.component.ts
+++ src/app/layout/blog/blog.component.ts
@@ -4,11 +4,12 @@ import { AsyncPipe, CommonModule, DatePipe, NgFor, NgIf } from '@angular/common'
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-blog',
   imports: [
-       AsyncPipe,
+    AsyncPipe,
     CommonModule,
     DatePipe,
     NgIf,
@@ -19,27 +20,27 @@ import { RouterLink } from '@angular/router';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent implements OnInit {
-
-
-  
   safeContent: SafeHtml = '';
   private blogService = inject(BlogService);
-  posts$ = this.blogService.getPosts();
-
-
+  
+  // Modified to sort posts by date in descending order
+  posts$ = this.blogService.getPosts().pipe(
+    map(posts => posts.sort((a, b) => {
+      const dateA = a.createdAt?.toDate().getTime() || 0;
+      const dateB = b.createdAt?.toDate().getTime() || 0;
+      return dateB - dateA; // For descending order (newest first)
+    }))
+  );
 
   constructor(
     private translate: TranslateService,
     private sanitizer: DomSanitizer
-
-
   ) { }
+
   useLanguage(language: string): void {
     this.translate.use(language);
   }
 
-
-  
   ngOnInit() {
     this.posts$.subscribe(posts => {
       if (posts.length > 0) {
@@ -47,5 +48,4 @@ export class BlogComponent implements OnInit {
       }
     });
   }
-
 }
