c9e0c50e5c3f9c78b36845c0c11b416193dba857
Pedro Jorge <skitvandarken2013@gmail.com>
2025-09-01 05:06:09 +0100

    banners traduziveis, looking glass add

--- src/app/layout/ad/ad.component.ts
+++ src/app/layout/ad/ad.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-ad',
-  imports: [],
+  imports: [ TranslatePipe],
   templateUrl: './ad.component.html',
   styleUrl: './ad.component.css'
 })
 export class AdComponent {
+  
+     constructor (private translate: TranslateService) {}
+      useLanguage(language: string): void {
+        this.translate.use(language);
+    }
 
 }
